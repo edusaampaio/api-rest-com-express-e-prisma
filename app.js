23c0ecd728d95b25b6ecd2e70af28c2698fa9625
Eduardo Sampaio <eduardosampaiio456@gmail.com>
2025-06-29 19:38:06 -0300

    feat: if para verificar se chegou algum dado no body

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
-// import express from 'express'
+import express from 'express'
 
-// const app = express()
-// app.use(express.json())
+const app = express()
+app.use(express.json())
 
-// app.li
+app.get('/', (req, res)=>{
+    res.send('Bem vindo ao meu server!')
+})
+
+app.post('/usuarios', async (req, res)=>{
+    const { email, name, age} = req.body
+
+    if(!email || !name || !age){
+       return res.status(400).json('você precisa inserir os dados.')
+    }else{
+       return res.status(201).json('Usário criado com sucesso!')
+    }
+
+})
+
+app.get('/usuario', (req, res)=>{
+    res.json()
+})
+
+app.listen(8080, ()=>{
+    console.log('servidor rodando na porta 8080');
+    
+})
